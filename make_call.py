b4411380c8066a1c9cf9feab4dc5817463ec01df
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-07 02:26:37 -0400

    fix: update 'make_call.py' and 'send_text.py' to use real credentials

--- make_call.py
+++ make_call.py
@@ -1,16 +1,13 @@
 from twilio.rest import Client
 from decouple import config
 
-TEST_ACCOUNT_SID = config("TEST_ACCOUNT_SID")
-TEST_AUTH_TOKEN = config("TEST_AUTH_TOKEN")
-
 ACCOUNT_SID = config("ACCOUNT_SID")
 AUTH_TOKEN = config("AUTH_TOKEN")
 
 MY_PHONE_NUMBER = config("MY_PHONE_NUMBER")
 MY_TWILIO_NUMBER = config("MY_TWILIO_NUMBER")
 
-client = Client(TEST_ACCOUNT_SID, TEST_AUTH_TOKEN)
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
 call = client.calls.create(
     url='http://demo.twilio.com/docs/voice.xml',
--- send_text.py
+++ send_text.py
@@ -1,20 +1,17 @@
 from twilio.rest import Client
 from decouple import config
 
-TEST_ACCOUNT_SID = config("TEST_ACCOUNT_SID")
-TEST_AUTH_TOKEN = config("TEST_AUTH_TOKEN")
-
 ACCOUNT_SID = config("ACCOUNT_SID")
 AUTH_TOKEN = config("AUTH_TOKEN")
 
 MY_PHONE_NUMBER = config("MY_PHONE_NUMBER")
 MY_TWILIO_NUMBER = config("MY_TWILIO_NUMBER")
 
-client = Client(TEST_ACCOUNT_SID, TEST_AUTH_TOKEN)
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
 message = client.messages.create(
     to=MY_PHONE_NUMBER, 
-    from_=MY_TEST_NUMBER,
+    from_=MY_TWILIO_NUMBER,
     body="Hello from Tico's MacBook Pro!")
 
 print(message.sid)
